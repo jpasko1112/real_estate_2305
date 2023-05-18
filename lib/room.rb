26f43b94193f528bb8699247f444c20ce70b0b37
Justin Pasko <jpasko1112@gmail.com>
2023-05-18 11:38:04 -0400

    Add methods/tests for 'is_painted?' and 'paint'.

--- lib/room.rb
+++ lib/room.rb
@@ -1,15 +1,27 @@
 class Room
   attr_reader :category,
               :length,
-              :width
+              :width,
+              :is_painted
+
+  # attr_accessor :is_painted
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+    @is_painted = false
   end
 
   def area
     @length * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
 RSpec.describe Room do
+  before(:each) do
+    @room1 = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
+  end
+
   describe "Iteration 1" do
     it "exists" do
-      room = Room.new(:bedroom, 10, '13')
-      expect(room).to be_a Room
-    end
+      expect(@room1).to be_a Room
+  end
 
     it "it has a category" do
-      room = Room.new(:bedroom, 10, '13')
+      @room = Room.new(:bedroom, 10, '13')
 
-      expect(room.category).to eq(:bedroom)
+      expect(@room.category).to eq(:bedroom)
     end
 
     it "can get area" do
-      room1 = Room.new(:bedroom, 10, '13')
-      room2 = Room.new(:living_room, 15, '12')
+      expect(@room1.area).to eq(130)
+      expect(@room2.area).to eq(180)
+    end
+  end
 
-      expect(room1.area).to eq(130)
-      expect(room2.area).to eq(180)
+  describe '#is_painted?' do
+    it 'is false by default' do
+      expect(@room1.is_painted?).to eq(false)
+    end
+  end
+
+  describe '#paint' do
+    it 'changes is_painted? to true' do
+      expect(@room1.is_painted?).to eq(false)
+      @room1.paint
+      expect(@room1.is_painted?).to eq(true)
     end
   end
 end
